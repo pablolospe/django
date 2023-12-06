894b0e0d7b46e02c90cfe1125feb664296fd0838
Pablo Xavier Lospennato <yosoypxl@gmail.com>
2023-12-06 21:33:55 +0000

    superuser

--- django_uno/build.sh
+++ django_uno/build.sh
@@ -9,7 +9,7 @@ python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
 
-DJANGO_SUPERUSER_USERNAME=andru \
+DJANGO_SUPERUSER_USERNAME=pablo \
 DJANGO_SUPERUSER_PASSWORD=1234 \
-DJANGO_SUPERUSER_EMAIL="andru@mail.com" \
+DJANGO_SUPERUSER_EMAIL="yosoypxl@gmail.com" \
 python manage.py createsuperuser --noinput
