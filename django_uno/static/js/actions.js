526cb0ad8cc1769c8d6d85b12fcad1018eb6937d
Pablo Xavier Lospennato <yosoypxl@gmail.com>
2023-12-06 02:53:24 +0000

    links a crypto

--- django_uno/static/js/actions.js
+++ django_uno/static/js/actions.js
@@ -8,50 +8,62 @@ fetch(endpoint)
 const diccionario = [
     {
         symbol: 'BTCUSDT',
+        link: 'https://bitcoin.org/',
         img: 'https://s2.coinmarketcap.com/static/img/coins/64x64/1.png'
     },
     {
         symbol: 'ETHUSDT',
+        link: 'https://ethereum.org/',
         img: 'https://s2.coinmarketcap.com/static/img/coins/64x64/1027.png'
     },
     {
         symbol: 'BNBUSDT',
+        link: 'https://www.bnbchain.org/',
         img: 'https://s2.coinmarketcap.com/static/img/coins/64x64/1839.png'
     },
     {
         symbol: 'XRPUSDT',
+        link: 'https://xrpl.org/',
         img: 'https://s2.coinmarketcap.com/static/img/coins/64x64/52.png'
     },
     {
         symbol: 'SOLUSDT',
+        link: 'https://solana.com/',
         img: 'https://s2.coinmarketcap.com/static/img/coins/64x64/5426.png'
     },
     {
         symbol: 'ADAUSDT',
+        link: 'https://cardano.org/',
         img: 'https://s2.coinmarketcap.com/static/img/coins/64x64/2010.png'
     },
     {
         symbol: 'DOGEUSDT',
+        link: 'https://dogecoin.com/',
         img: 'https://s2.coinmarketcap.com/static/img/coins/64x64/74.png'
     },
     {
         symbol: 'TRXUSDT',
+        link: 'https://tron.network/usdt',
         img: 'https://s2.coinmarketcap.com/static/img/coins/64x64/1958.png'
     },
     {
         symbol: 'GASUSDT',
+        link: 'https://es.tradingview.com/symbols/GASUSD/',
         img: 'https://s2.coinmarketcap.com/static/img/coins/64x64/1785.png'
     },
     {
         symbol: 'MATICUSDT',
+        link: 'https://polygon.technology/',
         img: 'https://s2.coinmarketcap.com/static/img/coins/64x64/3890.png'
     },
     {
         symbol: 'DOTUSDT',
+        link: 'https://www.binance.com/en/trade/DOT_USDT?type=spot',
         img: 'https://s2.coinmarketcap.com/static/img/coins/64x64/6636.png'
     },
     {
         symbol: 'LTCUSDT',
+        link: 'https://litecoin.com/',
         img: 'https://s2.coinmarketcap.com/static/img/coins/64x64/2.png'
     },
 ]
@@ -60,6 +72,7 @@ const mostrarData= (data)=>{
     // console.log(data);
 
     let filtro = data.filter(d => diccionario.some(entry => entry.symbol === d.symbol));
+    
 
     console.log(filtro);
 
@@ -67,10 +80,14 @@ const mostrarData= (data)=>{
     for (let i = 0; i < filtro.length; i++) {
 
         const img = diccionario.find(entry => entry.symbol === filtro[i].symbol).img;
+        const link = diccionario.find(entry => entry.symbol === filtro[i].symbol).link;
 
         symbol =`<div id="ETHUSDT" class="price-container">
-        <a href="https://www.linkedin.com" target="_blank"><img class="coin-logo" src=${img} alt="coin-logo"/></a>
-        <div class="row"><h3 >${filtro[i].symbol}</h3><p>Price:</p> <p> ${parseFloat(filtro[i].price).toFixed(2)}</p></div></div>`
+        <a href=${link} target="_blank">
+        <img class="coin-logo" src=${img} alt="coin-logo"/>
+        </a>
+        <div class="row">
+        <h3>${filtro[i].symbol}</h3><p>Price:</p> <p> ${parseFloat(filtro[i].price).toFixed(2)}</p></div></div>`
 
         document.getElementById('main').innerHTML += symbol
     }
