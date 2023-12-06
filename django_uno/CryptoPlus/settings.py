a1fa5c3254219b9f0cda352b00e8932ee6e6b6d6
L <53668256+ligarcia7@users.noreply.github.com>
2023-12-06 20:32:47 +0000

    adding allowed host

--- django_uno/CryptoPlus/settings.py
+++ django_uno/CryptoPlus/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-wbp1jgvrer&unk(zat$*&j6i&n8diyg#ujjnq@5i5nzlhxy*sr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ligarcia7.pythonanywhere.com']
 
 
 CUSTOM_APPS = [
