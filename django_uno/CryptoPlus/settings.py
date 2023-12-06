72334da0fd79335bc7457c29c86c2300d4328e96
L <53668256+ligarcia7@users.noreply.github.com>
2023-12-06 21:31:54 +0000

    change database

--- django_uno/CryptoPlus/settings.py
+++ django_uno/CryptoPlus/settings.py
@@ -80,11 +80,23 @@ WSGI_APPLICATION = "CryptoPlus.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'ligarcia7$db',
+            'USER': 'ligarcia7',
+            'PASSWORD': 'Lisandro',
+            'HOST': 'ligarcia7.mysql.pythonanywhere-services.com',
+            'PORT': '3306',
+        }
 }
 
 
