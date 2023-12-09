5d3c613cf29e11f41766c24452f56fc91c882539
Pablo Xavier Lospennato <yosoypxl@gmail.com>
2023-12-09 19:31:08 +0000

    saque django_api

--- django_api/django_api_users/users/serializers.py
+++
@@ -1,7 +0,0 @@
-# Definimos el modelo y los campos para la API
-from rest_framework import routers,serializers,viewsets
-from .models import User
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ['id', 'name', 'lastname', 'email', 'created_at']
